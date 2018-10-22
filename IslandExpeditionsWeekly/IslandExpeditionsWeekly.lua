a38ae6c54e80880b369b2cefa056ef7b5a6bf253
YngvarrC <marco.tordoor@hotmail.com>
2018-10-22 19:46:59 +0200

    Moved addon files in folder
    Added progress for weekly WQ completion
    
    Output is now split into Weekly azerite completion and weekly WQ completion

--- IslandExpeditionsWeekly/IslandExpeditionsWeekly.lua
+++ IslandExpeditionsWeekly/IslandExpeditionsWeekly.lua
@@ -34,12 +34,13 @@ addon:SetScript("OnEvent", function(self, event, arg1, ...)
             --    addon.panel:Show()
             elseif cmd == "reset" then
                 addon.initDB()
-                addon.printToChat("DB reset succesful. Please '/reload'")
+                addon.printToChat("DB reset succesful. Please '/reload' to fetch current character progress")
             end
         end
     elseif event == "PLAYER_ENTERING_WORLD" then
         addon.realm = GetRealmName()
         addon.name = UnitName("player")
+        addon.playerlevel = UnitLevel("player")
         addon.key = format("%s.%s", addon.realm, addon.name)
         addon.storeProgress()
     elseif event == "ISLAND_AZERITE_GAIN" then
@@ -54,30 +55,41 @@ function addon.initDB()
         --[[    ["Realm.Name"] = {
                 Realm = "some realm",
                 Name = "some name",
-                Progress = 0,
-                Finished = false
+                level = player level integer,
+                Finished_azerite = finished azerite quest?, 120 level only
+                Progress_azerite = progress azerite quest,
+                Finished_xp = finished weekly xp WQ? [110-119] level range
             } ]]
         }
     }
 end
 
 function addon.storeProgress()
-    local finished = IsQuestFlaggedCompleted(53435)
-    if finished == false then
+    -- track weekly azerite quest for 120+
+    local finished_azerite = IsQuestFlaggedCompleted(53435)
+    local progress_azerite = 0
+    if finished_azerite == false then
         local text, type, finished_fake, progress, needed = GetQuestObjectiveInfo(53435,1,false);
+        progress_azerite = progress
+    end
 
+    -- track weekly xp WQ for 110-119 characters
+    local finished_xp = nil
+    local faction, localizedfaction = UnitFactionGroup("player");
+    if faction == "Horde" then
+        finished_xp = IsQuestFlaggedCompleted(54166)
+    elseif faction == "Alliance" then
+        finished_xp = IsQuestFlaggedCompleted(54167)
+    end
+
+    if addon.playerlevel >= 110 then
         IslandExpeditionsWeeklyDB.Characters[addon.key] = {
             Realm = addon.realm,
             Name = addon.name,
-            Finished = finished,
-            Progress = progress
-        }
-    else
-        IslandExpeditionsWeeklyDB.Characters[addon.key] = {
-            Realm = addon.realm,
-            Name = addon.name,
-            Finished = finished,
-            Progress = nil
+            level = addon.playerlevel,
+            Finished_azerite = finished_azerite,
+            Progress_azerite = progress_azerite,
+            Finished_xp = finished_xp
         }
     end
 end
@@ -94,16 +106,33 @@ function addon.showProgress()
         end
         table.sort(ordered)
 
+        local line_max_header = "Weekly island azerite collecting progress:\n"
+        local line_max = ""
+        local line_xp_header = "Weekly island expedition WQ progress:\n"
+        local line_xp = ""
         for i = 1, #ordered do
-            local line = ""
             local v = IslandExpeditionsWeeklyDB.Characters[ordered[i]]
             local line = "\124c0000FFFF"..v.Realm.."\124r "..v.Name
-            if v.Finished then
-                line = line.." \124cFF00FF00Finished!\124r"
-            else
-                line = line.." \124cFFFF0000"..v.Progress.."/40000\124r"
+            if v.level == 120 then
+                if v.Finished_azerite then
+                    line_max = line_max..line.." \124cFF00FF00Finished collecting azerite!\124r\n"
+                else
+                    line_max = line_max..line.." \124cFFFF0000"..v.Progress_azerite.."/40000 azerite collected\124r\n"
+                end
+            elseif v.level >= 110 and v.level <= 119 then
+                if v.Finished_xp == true then
+                    line_xp = line_xp..line.." \124cFF00FF00Finished xp quest!\124r\n"
+                elseif v.Finished_xp == false then
+                    line_xp = line_xp..line.." \124cFFFF0000Xp quest not completed!\124r\n"
+                end
             end
-            addon.printToChat(line);
+        end
+
+        if string.len(line_max) > 0 then
+            addon.printToChat(line_max_header..line_max);
+        end
+        if string.len(line_xp) > 0 then
+            addon.printToChat(line_xp_header..line_xp);
         end
     else
         addon.printToChat("no data!")
