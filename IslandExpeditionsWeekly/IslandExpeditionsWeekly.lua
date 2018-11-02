8f93ca43503ebb2b93a54e0c021f11ba2fac6ce3
YngvarrC <marco.tordoor@hotmail.com>
2018-11-02 14:54:49 +0100

    Addon now works on Ace3 framework
    Added ignore character feature
    
    Reorganized printed progress list

--- IslandExpeditionsWeekly/IslandExpeditionsWeekly.lua
+++ IslandExpeditionsWeekly/IslandExpeditionsWeekly.lua
@@ -6,151 +6,196 @@
 ==============
 --]]
 
+-- Init Ace3
+local addonName = ...
+_G[addonName] = LibStub("AceAddon-3.0"):NewAddon(addonName, "AceConsole-3.0", "AceEvent-3.0")
+--_G[addonName].GUI = LibStub("AceGUI-3.0")
+--_G[addonName].LDB = LibStub("LibDataBroker-1.1")
+--_G[addonName].LDBI = LibStub("LibDBIcon-1.0")
+
+local addon = _G[addonName]
+
+local chat_options = {
+    type = "group",
+    args = {
+        --[[show_old = {
+            type = "execute",
+            name = 'show',
+            desc = "Show the info in chat the old way",
+            func = function() addon:showProgressOld() end,
+            order = 6
+        },--]]
+        show = {
+            type = "execute",
+            name = 'show',
+            desc = "Show the info in chat",
+            func = function() addon:showProgress() end,
+            order = 1
+        },
+        reset = {
+            type = "execute",
+            name = 'reset',
+            desc = "Reset the character database",
+            func = function() addon:initDB(); addon:storeProgress(); addon:printToChat("DB reset succesful") end,
+            order = 5
+        },
+        ignorelist = {
+            type = "execute",
+            name = 'ignorelist',
+            desc = "Get a list of all ignored characters",
+            func = function() addon:printIgnoreList() end,
+            order = 3
+        },
+        clearignore = {
+            type = "execute",
+            name = 'clearignore',
+            desc = "Unignore all characters",
+            func = function() addon:clearIgnoreList() end,
+            order = 4
+        },
+        ignore = {
+            type = "input",
+            name = "toggleignore",
+            usage = "<Realm>.<Character>",
+            desc = "Toggle ignoring a certain character so it doesn't get displayed",
+            get = false,
+            set = function(info, character_key) addon:toggleIgnoreCharacter(character_key) end,
+            order = 2
+        },
+        --[[data = {
+            type = "execute",
+            name = 'data',
+            func = function() addon:setData(); addon:printToChat("DB data fill succesful") end
+        },]]
+    },
+}
+
 -- localize used functions
-local IsQuestFlaggedCompleted, GetQuestObjectiveInfo, GetRealmName, UnitName, UnitLevel, GetCurrentRegion =
-    IsQuestFlaggedCompleted, GetQuestObjectiveInfo, GetRealmName, UnitName, UnitLevel, GetCurrentRegion
+local IsQuestFlaggedCompleted = _G.IsQuestFlaggedCompleted
+local GetQuestObjectiveInfo = _G.GetQuestObjectiveInfo
+local GetRealmName = _G.GetRealmName
+local UnitName = _G.UnitName
+local UnitLevel = _G.UnitLevel
+local GetCurrentRegion = _G.GetCurrentRegion
+local tinsert = table.insert
+local tsort = table.sort
 
-local addon = CreateFrame("FRAME", "IslandExpeditionsWeekly", UIParent);
-addon:RegisterEvent("ADDON_LOADED")
-addon:RegisterEvent("ISLAND_AZERITE_GAIN")
-addon:RegisterEvent("PLAYER_ENTERING_WORLD")
-addon:RegisterEvent("PLAYER_LEVEL_UP")
+function addon:OnInitialize()
+    LibStub("AceConfig-3.0"):RegisterOptionsTable(addonName, chat_options, {"IslandExpeditionsWeekly", "iew"})
 
-addon:SetScript("OnEvent", function(self, event, arg1, ...)
-    if event == "ADDON_LOADED" and arg1 == "IslandExpeditionsWeekly" then
-        if IslandExpeditionsWeeklyDB == nil then
-            addon.initDB()
-        end
-
-        -- determine server reset day based on region
-        local region = GetCurrentRegion()
-        addon.resetday = {}
-        if region == 1 then
-            addon.resetday["2"] = true -- US resets on tuesday
-        elseif region == 3 then
-            addon.resetday["3"] = true -- EU resets on wednesday
-        elseif region == 2 or region == 4 or region == 5 then -- Korea, Taiwan, China
-            addon.resetday["4"] = true --  resets on thursday
-        end
-
-        addon.next_reset = addon.getNextWeeklyReset()
-        -- if previous reset is earlier than next reset, reset manually
-        if IslandExpeditionsWeeklyDB.ResetTime < addon.next_reset then
-            addon.resetProgress()
-        end
-
-        -- Slash Command List
-        SLASH_IslandExpeditionsWeekly1 = "/islandexpeditionsweekly";
-        SLASH_IslandExpeditionsWeekly2 = "/iew";
-        SlashCmdList["IslandExpeditionsWeekly"] = function(cmd)
-            if not cmd or cmd == "" then
-                local text = "List of available commands:"..
-                "\n\124c0000FFFFshow\124r: Show the progress for the weekly on all characters (Note: Only those logged in since using the addon)"..
-                "\n\124c0000FFFFreset\124r: Reset the database. This will clear all your characters progress!"..
-                "\nNote that both /islandexpeditionsweekly and /iew work for slash commands"
-                addon.printToChat(text)
-            elseif cmd == "show" then
-                addon.showProgress()
-            --    addon.panel:Show()
-            elseif cmd == "reset" then
-                addon.initDB()
-                addon.printToChat("DB reset succesful. Please '/reload' to fetch current character progress")
-            end
-        end
-    elseif event == "PLAYER_ENTERING_WORLD" then
-        addon.realm = GetRealmName()
-        addon.name = UnitName("player")
-        addon.playerlevel = UnitLevel("player")
-        addon.key = format("%s.%s", addon.realm, addon.name)
-        addon.storeProgress()
-    elseif event == "ISLAND_AZERITE_GAIN" then
-        addon.storeProgress()
-    elseif event == "PLAYER_LEVEL_UP" then
-        addon.playerlevel = UnitLevel("player")
-        addon.storeProgress()
+    if IslandExpeditionsWeeklyDB == nil then
+        addon:initDB()
     end
-end)
 
+    if IslandExpeditionsWeeklyDB.IgnoredCharacters == nil then
+        IslandExpeditionsWeeklyDB.IgnoredCharacters = {}
+    end
 
-function addon.initDB()
-    IslandExpeditionsWeeklyDB = {
-        ResetTime = 0, -- timestamp of upcoming reset
-        Characters = {
-        --[[    ["Realm.Name"] = {
-                Realm = "some realm",
-                Name = "some name",
-                level = player level integer,
-                Finished_azerite = finished azerite quest?, 120 level only
-                Progress_azerite = progress azerite quest,
-                Finished_xp = finished weekly xp WQ? [110-119] level range
-            } ]]
-        }
-    }
-end
-
-function addon.storeProgress()
-    if addon.playerlevel >= 110 then
-
-        -- track weekly azerite quest for 120+
-        local finished_azerite = false
-        local progress_azerite = 0
-        if addon.playerlevel == 120 then
-            finished_azerite = IsQuestFlaggedCompleted(53435)
-            if finished_azerite == false then
-                local text, type, finished_fake, progress, needed = GetQuestObjectiveInfo(53435,1,false);
-                progress_azerite = progress
-            end
-        end
-
-        -- track weekly xp WQ for 110-119 characters
-        local finished_xp = true
-        if addon.playerlevel > 109 and addon.playerlevel < 120 then
-            local faction, _ = UnitFactionGroup("player");
-            if faction == "Horde" then
-                finished_xp = IsQuestFlaggedCompleted(54166)
-            elseif faction == "Alliance" then
-                finished_xp = IsQuestFlaggedCompleted(54167)
-            end
-        end
-
-        IslandExpeditionsWeeklyDB.Characters[addon.key] = {
-            Realm = addon.realm,
-            Name = addon.name,
-            level = addon.playerlevel,
-            Finished_azerite = finished_azerite,
-            Progress_azerite = progress_azerite,
-            Finished_xp = finished_xp
-        }
+    addon.next_reset = addon:getNextWeeklyReset()
+    -- if previous reset is earlier than next reset, reset manually
+    if IslandExpeditionsWeeklyDB.ResetTime < addon.next_reset then
+        addon:resetProgress()
     end
 end
 
-function addon.resetProgress()
-    for key, data in pairs(IslandExpeditionsWeeklyDB.Characters) do
-        -- Only reset those that finished the weekly because the weekly carries over to next week
-        if IslandExpeditionsWeeklyDB.Characters[key].Finished_azerite == true then
-            IslandExpeditionsWeeklyDB.Characters[key].Finished_azerite = false
-            IslandExpeditionsWeeklyDB.Characters[key].Progress_azerite = 0
+function addon:OnEnable()
+    self:RegisterEvent("PLAYER_ENTERING_WORLD")
+    self:RegisterEvent("PLAYER_LEVEL_UP")
+    self:RegisterEvent("ISLAND_AZERITE_GAIN")
+end
+
+function addon:OnDisable()
+    self:UnregisterAllEvents()
+end
+
+-- EVENT processing functions
+function addon:PLAYER_ENTERING_WORLD()
+    addon.realm = GetRealmName()
+    addon.name = UnitName("player")
+    addon.playerlevel = UnitLevel("player")
+    addon.key = format("%s.%s", addon.realm, addon.name)
+    addon:storeProgress()
+end
+function addon:PLAYER_LEVEL_UP()
+    addon.playerlevel = UnitLevel("player")
+    addon:storeProgress()
+end
+function addon:ISLAND_AZERITE_GAIN()
+    addon:storeProgress()
+end
+-- END EVENT processing functions
+
+-- handle chat commands
+function addon:ChatCommand(input)
+  if not input or input:trim() == "" then
+    -- this will show an options dialog at some point
+    LibStub("AceConfigDialog-3.0"):Open("IslandExpeditionsWeeklyOptions")
+  else
+    LibStub("AceConfigCmd-3.0").HandleCommand(addon, "iew", "IslandExpeditionsWeeklyOptions", input)
+  end
+end
+
+--[[
+====== Print stuff to user functions
+--]]
+
+function addon:showProgress()
+    if IslandExpeditionsWeeklyDB ~= nil then
+        local chars_120 = {}
+        local chars_other = {}
+        for key, char in pairs(IslandExpeditionsWeeklyDB.Characters) do
+            if IslandExpeditionsWeeklyDB.IgnoredCharacters[key] == nil then
+                if char.level == 120 then
+                    tinsert(chars_120, key)
+                else
+                    tinsert(chars_other, key)
+                end
+            end
+        end
+        -- sort both alphabetically
+        tsort(chars_120)
+        tsort(chars_other)
+        local ordered_chars = addon:mergeTables(chars_120, chars_other)
+
+        local data_header = "Weekly island expedition progress:\nRealm Name: Azerite Collected -- WQ\n"
+        local data_lines = ""
+        for i = 1, #ordered_chars do
+            local v = IslandExpeditionsWeeklyDB.Characters[ordered_chars[i]]
+            local line = "\124c0000FFFF"..v.Realm.."\124r "..v.Name..":"
+            if v.level == 120 then
+                -- only 120s can do the azerite collecting
+                if v.Finished_azerite then
+                    line = line.." \124cFF00FF00Done\124r"
+                else
+                    line = line.." \124cFFFF0000"..v.Progress_azerite.."/40K\124r"
+                end
+            else
+                line = line.." \124c0000FFFFN/A\124r"
+            end
+
+            -- everyone can do the world quest
+            if v.Finished_xp == true then
+                line = line.." -- \124cFF00FF00Done\124r"
+            elseif v.Finished_xp == false then
+                line = line.." -- \124cFFFF0000Open\124r"
+            end
+            data_lines = data_lines..line.."\n"
         end
 
-        if IslandExpeditionsWeeklyDB.Characters[key].Finished_xp == true then
-            IslandExpeditionsWeeklyDB.Characters[key].Finished_xp = false
-        end
+        addon:printToChat(data_header..data_lines);
+    else
+        addon:printToChat("no data!")
     end
-
-    IslandExpeditionsWeeklyDB.ResetTime = addon.next_reset
 end
 
-function addon.printToChat(text)
-    DEFAULT_CHAT_FRAME:AddMessage("\124cFFFFFF00[IEW] "..text.."\124r")
-end
-
-function addon.showProgress()
+-- DEPRECATED
+function addon:showProgressOld()
     if IslandExpeditionsWeeklyDB ~= nil then
         local ordered = {}
         for key in pairs(IslandExpeditionsWeeklyDB.Characters) do
-            table.insert(ordered, key)
+            tinsert(ordered, key)
         end
-        table.sort(ordered)
+        tsort(ordered)
 
         local line_max_header = "Weekly island azerite collecting progress:\n"
         local line_max = ""
@@ -175,35 +220,192 @@ function addon.showProgress()
         end
 
         if #line_max > 0 then
-            addon.printToChat(line_max_header..line_max);
+            addon:printToChat(line_max_header..line_max);
         end
         if #line_xp > 0 then
-            addon.printToChat(line_xp_header..line_xp);
+            addon:printToChat(line_xp_header..line_xp);
         end
     else
-        addon.printToChat("no data!")
+        addon:printToChat("no data!")
     end
 end
 
-function addon.createUI()
-    local panel = CreateFrame("Frame", "IslandExpeditionsWeeklyFrame", UIParent, "UIPanelDialogTemplate")
-    panel:Hide()
-    panel:SetPoint("LEFT", 20, 0)
-    panel:SetSize(300, 500)
-    panel:EnableMouse(true)
-    panel:SetMovable(true)
-    panel:SetToplevel(true)
-    panel:RegisterForDrag("LeftButton")
-    panel:HookScript("OnDragStart", function(self) self:StartMoving() end)
-    panel:HookScript("OnDragStop", function(self) self:StopMovingOrSizing() end)
+function addon:printIgnoreList()
+        local list = ""
+        for key in pairs(IslandExpeditionsWeeklyDB.IgnoredCharacters) do
+            local parts = addon:split(key, ".")
+            if #parts == 2 then
+                list = list.."\124c0000FFFF"..parts[1].."\124r "..parts[2].."\n"
+            end
+        end
+        if #list > 0 then
+            addon:printToChat("Currenlty ignoring:\n"..list)
+        else
+            addon:printToChat("No characters ignored")
+        end
+end
 
-    panel.Title:SetText("Island Expeditions Weekly Progress")
+--[[
+====== DB interaction functions
+--]]
+-- store a default table into the savedvariable
+function addon:initDB()
+    IslandExpeditionsWeeklyDB = {
+        ResetTime = 0, -- timestamp of upcoming reset
+        Characters = {
+        --[[    ["Realm.Name"] = {
+                Realm = "some realm",
+                Name = "some name",
+                level = player level integer,
+                Finished_azerite = finished azerite quest?, 120 level only
+                Progress_azerite = progress azerite quest,
+                Finished_xp = finished weekly xp WQ? [110-119] level range
+            } ]]
+        },
+        IgnoredCharacters = {
+            -- simple list of Realm.Name as key to store characters we do not want to show
+            -- "Realm.Name" = true
+        }
+    }
+    -- update reset time to next reset
+    addon:saveResetTime()
+end
 
-    return panel
+function addon:saveResetTime()
+    if addon.next_reset ~= nil then
+        IslandExpeditionsWeeklyDB.ResetTime = addon.next_reset
+    else
+        IslandExpeditionsWeeklyDB.ResetTime = 0
+    end
+end
+
+-- store/update progress on current character
+function addon:storeProgress()
+    if addon.playerlevel >= 110 then
+
+        -- track weekly azerite quest for 120+
+        local finished_azerite = false
+        local progress_azerite = 0
+        if addon.playerlevel == 120 then
+            finished_azerite = IsQuestFlaggedCompleted(53435)
+            if finished_azerite == false then
+                local text, type, finished_fake, progress, needed = GetQuestObjectiveInfo(53435,1,false);
+                progress_azerite = progress
+            end
+        end
+
+        -- track weekly WQ
+        local finished_xp = true
+        local faction, _ = UnitFactionGroup("player");
+        if faction == "Horde" then
+            finished_xp = IsQuestFlaggedCompleted(54166)
+        elseif faction == "Alliance" then
+            finished_xp = IsQuestFlaggedCompleted(54167)
+        end
+
+        IslandExpeditionsWeeklyDB.Characters[addon.key] = {
+            Realm = addon.realm,
+            Name = addon.name,
+            level = addon.playerlevel,
+            Finished_azerite = finished_azerite,
+            Progress_azerite = progress_azerite,
+            Finished_xp = finished_xp
+        }
+    end
+end
+
+-- reset savedvariable to a 'nothing completed' state
+function addon:resetProgress()
+    for key, data in pairs(IslandExpeditionsWeeklyDB.Characters) do
+        -- Only reset those that finished the weekly azerite because it carries over to next week
+        if IslandExpeditionsWeeklyDB.Characters[key].Finished_azerite == true then
+            IslandExpeditionsWeeklyDB.Characters[key].Finished_azerite = false
+            IslandExpeditionsWeeklyDB.Characters[key].Progress_azerite = 0
+        end
+
+        -- reset WQ for everyone
+        if IslandExpeditionsWeeklyDB.Characters[key].Finished_xp == true then
+            IslandExpeditionsWeeklyDB.Characters[key].Finished_xp = false
+        end
+    end
+
+    addon:saveResetTime()
+end
+
+-- (un)ignore a character
+function addon:toggleIgnoreCharacter(character_key)
+    if IslandExpeditionsWeeklyDB.Characters[character_key] == nil then
+        addon:printToChat("Character '"..character_key.."' is not being tracked. Unable to ignore")
+    else
+        if IslandExpeditionsWeeklyDB.IgnoredCharacters[character_key] == nil then
+            IslandExpeditionsWeeklyDB.IgnoredCharacters[character_key] = true
+            addon:printToChat("Ignoring "..character_key)
+        else
+            IslandExpeditionsWeeklyDB.IgnoredCharacters[character_key] = nil
+            addon:printToChat("No longer ignoring "..character_key)
+        end
+    end
+end
+
+function addon:clearIgnoreList()
+    IslandExpeditionsWeeklyDB.IgnoredCharacters = {}
+    addon:printToChat("Ignore list cleared")
+end
+
+--[[
+====== Utility functions
+--]]
+function addon:printToChat(text)
+    DEFAULT_CHAT_FRAME:AddMessage("\124cFFFFFF00[IEW] "..text.."\124r")
+end
+
+function addon:mergeTables(t1, t2)
+    local resulting_table = {}
+    if t1 ~= nil and t2 ~= nil then
+        for _, value in pairs(t1) do
+            tinsert(resulting_table, value)
+        end
+        for _, value in pairs(t2) do
+            tinsert(resulting_table, value)
+        end
+    end
+
+    return resulting_table
+end
+
+function addon:split(input, separator)
+    -- default split on whitespace
+    if separator == nil then
+        separator = "%s"
+    end
+
+    local list = {}
+    for part in string.gmatch(input, "([^"..separator.."]+)") do
+        tinsert(list, part)
+    end
+
+    return list
 end
 
 -- Get next weekly reset, server time
-function addon.getNextWeeklyReset()
+function addon:getNextWeeklyReset()
+    if addon.resetday == nil then
+        -- determine server reset day based on region
+        local region = GetCurrentRegion()
+        addon.resetday = {}
+        if region ~= nil then
+            if region == 1 then
+                addon.resetday["2"] = true -- US resets on tuesday
+            elseif region == 3 then
+                addon.resetday["3"] = true -- EU resets on wednesday
+            elseif region == 2 or region == 4 or region == 5 then -- Korea, Taiwan, China
+                addon.resetday["4"] = true --  resets on thursday
+            end
+        else
+            addon.resetday["3"] = true -- fallback on eu reset
+        end
+    end
+
     -- get daily reset time
     local daily_reset_ts = GetQuestResetTime()
     local server_time = C_Calendar.GetDate()
@@ -225,4 +427,8 @@ function addon.getNextWeeklyReset()
     end
 
     return reset
+end
+
+function addon:setData()
+    -- debug function to fill up the db variable
 end
--- README.md
+++ README.md
@@ -3,14 +3,14 @@
 Tired of always having to return to that table to check your progress? So was I and that's why I made this little addon.
 As soon as you log in with a character or collect azerite in an island your progress will be fetched and saved.
 
-The addon will also track your weekly world quest completion for winning an island expedition for character between levels 110 and 119.
- 
-## Usage
+The addon will also track your weekly world quest completion for winning an island expedition.
 
-Show the progress of all your characters (levels 110+) in chat via command:
+Currently only via chat commands and prints in the default chat window. I'll get to implementing a UI for this at some point.
 
-    /iew show
-
- Reset the database incase of errors due to changed datastructures (afterwards you will  have to log into each character again so the addon can fetch their progress)
-
-    /iew reset
+## Available commands
+All commands work with either `/IslandExpeditionsWeekly` or `/iew`
+* `/iew show` Show the progress of all your characters (levels 110+)
+* `/iew ignore Realm.Name` Ignore a character so it will not be displayed. Using the command again will unignore. Example: if I want to ignore character `Abc` on realm `Xyz`, I have to use the command `/iew ignore Xyz.Abc`
+* `/iew ignorelist` Show a list of all ignored characters
+* `/iew clearignore` Erase the ignore list
+* `/iew reset` Reset the database of the addon. This should be the same as doing a complete reinstall. Might be needed if things go south
